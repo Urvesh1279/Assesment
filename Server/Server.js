f998d143c8244f7560c316886404e83f64210478
Urvesh1279 <urveshmukeshbhai1279@gmail.com>
2024-06-11 15:59:29 +0530

    fainal

--- Server/Server.js
+++ Server/Server.js
@@ -5,11 +5,14 @@ const routerUi = require("../Router/Ui/Routerui.js");
 const bodyparsor = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const http = require("http"); 
+const WebSocket = require("ws"); 
 
 const servermanager = {};
 
 servermanager.prepare = () => {
-  servermanager.server = libexpress();
+  const app = libexpress();
+  servermanager.server = http.createServer(app); 
 
   servermanager.server.use(libexpress.static("public"));
 
@@ -36,6 +39,24 @@ servermanager.prepare = () => {
   servermanager.server.use((req, res) => {
     res.status(200).json({ error: "No Such Api" });
   });
+
+
+  const wss = new WebSocket.Server({ server: servermanager.server });
+
+  wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('message', (message) => {
+      console.log(`Received message: ${message}`);
+      // Echo the message back to the client
+      ws.send(`Server received: ${message}`);
+    });
+
+    ws.on('close', () => {
+      console.log('Client disconnected');
+    });
+  });
+
+  servermanager.wss = wss; 
 };
 
 servermanager.start = () => {
